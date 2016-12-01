aa5d7cf9bbe054d71c262375a135d5773a9d3ec1
Belyea, Leah <lbelyea@cvent.com>
2016-12-01 10:16:07 -0400

    day 1 puzzle 1 refactor

--- 1.py
+++ 1.py
@@ -14,30 +14,18 @@ for instruction in instructions:
     turn = instruction[0]
     steps = int(instruction[1:])
 
-    if turn == 'L':
-        currentDirection = currentDirection - 90
-    if turn == 'R':
-        currentDirection = currentDirection + 90
-    if currentDirection >= 360:
-        currentDirection = currentDirection - 360
-    if currentDirection < 0:
-        currentDirection = currentDirection + 360
-    print turn
-    print currentDirection
+    currentDirection = currentDirection - 90 if turn == 'L' else currentDirection + 90
+    currentDirection = currentDirection - 360 if currentDirection >= 360 else currentDirection
+    currentDirection = currentDirection + 360 if currentDirection < 0 else currentDirection
 
-    print instruction
     if currentDirection == 0:
         currentLocation = [currentLocation[0] + steps, currentLocation[1]]
-    if currentDirection == 90:
+    elif currentDirection == 90:
         currentLocation = [currentLocation[0], currentLocation[1] + steps]
-    if currentDirection == 180:
+    elif currentDirection == 180:
         currentLocation = [currentLocation[0] - steps, currentLocation[1]]
-    if currentDirection == 270:
+    elif currentDirection == 270:
         currentLocation = [currentLocation[0], currentLocation[1] - steps]
 
-    print currentLocation
-
 taxicabDistance = abs(currentLocation[0]) + abs(currentLocation[1])
-# print currentLocation
-
 print taxicabDistance
