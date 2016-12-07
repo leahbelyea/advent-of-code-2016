accbabce2a774d3fb435144a8fe8a6a15747e14e
Leah Belyea <leahbelyea@gmail.com>
2016-12-07 07:52:26 -0400

    day 7 partial part 2

--- 7.py
+++ 7.py
@@ -10,6 +10,15 @@ with open('input/7.txt', 'r') as f:
 #     'ioxxoj[asdfgh]zxcvbn'
 # ]
 
+input = [
+    'aba[bab]xyz',
+    'xyx[xyx]xyx',
+    'aaaxyx[kek]eke',
+    'zazbz[bzb]cdb'
+]
+
+# Part 1
+
 supportingIps = []
 
 for ip in input:
@@ -36,3 +45,41 @@ for ip in input:
         supportingIps.append(ip)
 
 print len(supportingIps)
+
+
+# Part 2
+
+supportingIps = []
+
+for ip in input:
+    ipSupernet = re.sub('\[[a-z]*\]', '', ip)
+
+    abaMatches = re.findall('([a-z])([a-z])\\1', ipSupernet) or []
+    abaValid = False
+    abas = []
+    for aba in abaMatches:
+        if aba[0] != aba[1]:
+            abaValid = True
+            if aba not in abas: abas.append(aba)
+
+    hasAba = len(abaMatches) > 0 and abaValid
+    print ip
+    print abas
+    print '\n\n'
+
+    #
+    # abbaInBracketsMatches = re.findall('\[[a-z]*([a-z])([a-z])\\1[a-z]*\]', ip) or []
+    #
+    # abbaInBracketsValid = False
+    # for abba in abbaInBracketsMatches:
+    #     if abba[0] != abba[1]:
+    #         abbaInBracketsValid = True
+    #
+    # hasAbbaInBrackets = len(abbaInBracketsMatches) > 0 and abbaInBracketsValid
+    #
+    # supportsTls = hasAbba and not hasAbbaInBrackets
+    #
+    # if supportsTls:
+    #     supportingIps.append(ip)
+
+# print len(supportingIps)
